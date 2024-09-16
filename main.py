9c387e5c2551324b54ab00637ac1e4330fc113fc
Sahil Uddin Ishmam <sahiluddinishmam@gmail.com>
2024-09-16 23:35:21 +0600

    main function added

---
+++ main.py
@@ -0,0 +1,13 @@
+""" 
+    Simple Calculator using Python
+
+Author : Sahil Uddin Ishmam
+Department : CSE
+University : Metropolitan University
+
+"""
+
+# Main function
+if __name__ == '__main__':
+    pass
+
