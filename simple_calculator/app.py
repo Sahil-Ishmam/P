4699654009a04132fa0cfd909ea4ae48fd6e5fcd
Sahil Uddin Ishmam <sahiluddinishmam@gmail.com>
2024-09-16 23:39:55 +0600

    operation function has written

--- simple_calculator/app.py
+++ simple_calculator/app.py
@@ -7,6 +7,31 @@ University : Metropolitan University
 
 """
 
+
+# function definition
+
+def addition(a,b):
+    return a + b
+
+def subtraction(a,b):
+    return a - b
+
+def multiplication(a,b):
+    return a * b
+
+def division(a,b):
+    try :
+        return a/b
+    except Exception as error:
+        return error
+
+def modulus(a,b):
+    try :
+        return a % b 
+    except Exception as error:
+        return error
+
+
 # Main function
 if __name__ == '__main__':
     pass
