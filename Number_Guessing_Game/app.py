e03aa41d840542dfe62ada82f8b4eb76d877fb3e
Sahil Uddin Ishmam <sahiluddinishmam@gmail.com>
2024-09-17 14:22:43 +0600

    Main function added

---
+++ Number_Guessing_Game/app.py
@@ -0,0 +1,15 @@
+""" 
+    Simple Calculator using Python
+
+Author : Sahil Uddin Ishmam
+Department : CSE
+University : Metropolitan University
+
+
+"""
+
+
+
+
+if __name__ == '__main__':
+    pass
